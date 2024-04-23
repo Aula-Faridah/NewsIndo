3d7580f0f9129b095b90a0cb35826620dab905d9
Aula Faridah <aulafaridah@gmail.com>
2024-04-23 13:07:45 +0700

    API Zetizen Service Fetch News

--- NewsIndo/Services/Constant.swift
+++ NewsIndo/Services/Constant.swift
@@ -10,5 +10,9 @@ import Foundation
 struct Constant {
     static let newsUrl = "https://berita-indo-api-next.vercel.app/api/kumparan-news/"
     
+    static let zetizenUrl = "https://berita-indo-api-next.vercel.app/api/zetizen-jawapos-news/movie"
+    
     static let newsTitle = "Berita Indo"
+    
+    static let zetizenTitle = "Zetizen's Movies"
 }
---
+++ NewsIndo/Zetizen/Services/APIZetizenService.swift
@@ -0,0 +1,32 @@
+//
+//  APIZetizenService.swift
+//  NewsIndo
+//
+//  Created by MacBook Pro on 23/04/24.
+//
+
+import Foundation
+import Alamofire
+
+class APIZetizenService {
+    static let shared = APIZetizenService()
+    private init(){}
+    
+    func fetchNews() async throws -> [Movies] {
+        guard let url = URL(string: Constant.zetizenUrl) else {
+            print("😡 ERROR: Could not convert \(String(describing: URL(string: Constant.zetizenUrl))) to a URL")
+            throw URLError(.badURL)}
+        
+        let news = try await withCheckedThrowingContinuation { continuation in
+            AF.request(url).responseDecodable(of: NewsZetizen.self) { response in
+                switch response.result{
+                case.success(let newResponse):
+                    continuation.resume(returning: newsResponse.data)
+                case.failure(let err):
+                    continuation.resume(throwing: err)
+                }
+            }
+        }
+        return news
+    }
+}
